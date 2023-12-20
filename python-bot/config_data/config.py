d213becafdaa7076d79a5aba56198f6c5a51e602
Ilya Ibraev <shilya91@gmail.com>
2023-12-20 04:30:20 +0300

    base functional

---
+++ python-bot/config_data/config.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class DatabaseConfig:
+    database: str         # Название базы данных
+    db_host: str          # URL-адрес базы данных
+    db_user: str          # Username пользователя базы данных
+    db_password: str      # Пароль к базе данных
+
+
+@dataclass
+class TgBot:
+    token: str            # Токен для доступа к телеграм-боту
+    admin_ids: list[int]  # Список id администраторов бота
+
+@dataclass
+class PersonalPUBChemAPI:
+    url: str
+    port: str
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    db: DatabaseConfig
+    api: PersonalPUBChemAPI
+
+
+def load_config(path: str | None = None) -> Config:
+
+    env: Env = Env()
+    env.read_env(path)
+
+    return Config(
+        tg_bot=TgBot(
+            token=env('BOT_TOKEN'),
+            admin_ids=list(map(int, env.list('ADMIN_IDS')))
+        ),
+        db=DatabaseConfig(
+            database=env('DATABASE'),
+            db_host=env('DB_HOST'),
+            db_user=env('DB_USER'),
+            db_password=env('DB_PASSWORD')
+        ),
+        api=PersonalPUBChemAPI(
+            url=env('API_URL'),
+            port=env('API_PORT')
+        )
+    )
