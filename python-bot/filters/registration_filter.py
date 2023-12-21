147c8704ec4bcb0647ae16e333047eded582b7de
Ilya Ibraev <shilya91@gmail.com>
2023-12-21 03:02:18 +0300

    adding comments for state

--- python-bot/filters/registration_filter.py
+++ python-bot/filters/registration_filter.py
@@ -2,7 +2,5 @@ from aiogram.fsm.state import default_state, State, StatesGroup
 
 
 class RegistrationForm(StatesGroup):
-    # Создаем экземпляры класса State, последовательно
-    # перечисляя возможные состояния, в которых будет находиться
-    # бот в разные моменты взаимодейтсвия с пользователем
-    registration_completed = State()        # Состояние ожидания регистрации
+    # Состояние зарегистрированного в DB пользователя
+    registration_completed = State()  
