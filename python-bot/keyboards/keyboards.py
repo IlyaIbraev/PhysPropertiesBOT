7cf6751182ac3e78b7f13e839743cb4e3e230c80
Ilya Ibraev <shilya91@gmail.com>
2023-12-21 03:19:19 +0300

    adding comments, improving stability

--- python-bot/keyboards/keyboards.py
+++ python-bot/keyboards/keyboards.py
@@ -1,14 +1,11 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from lexicon.lexicon_ru import LEXICON_RU
-
 def create_molecule_keyboard(response_data: dict[str]):
-    
+    # Инициализация создателя клавиатуры
     kb_builder = InlineKeyboardBuilder()
-
+    # Формирование списка кнопок
     buttons: list[InlineKeyboardButton] = []
-
     for property in response_data["Properties"]:
         buttons.append(
             InlineKeyboardButton(
@@ -16,5 +13,7 @@ def create_molecule_keyboard(response_data: dict[str]):
                 callback_data="select:{}:{}".format(response_data["CID"], property)
             )
         )
+    # Их добавление в клавиатуру
     kb_builder.row(*buttons, width=2)
+    # Возвращение кнопок в виде list[list][button]
     return kb_builder.as_markup()
