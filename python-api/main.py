8015b9e3f49f4095fb9851bf3aae1dd9ee919e00
Ilya Ibraev <shilya91@gmail.com>
2023-12-21 08:27:38 +0300

    responses saving

--- python-api/main.py
+++ python-api/main.py
@@ -2,18 +2,43 @@ import aiohttp
 from fastapi import FastAPI, HTTPException
 import requests
 from typing import Literal
-import asyncio
+import asyncpg
+import json
 
 app = FastAPI()
 
+
 @app.get("/properties_from_cid/{cid}")
 async def get_properties_from_cid(cid: int) -> dict:
+    conn = await asyncpg.connect('postgresql://postgres:password@localhost/pubchem')
+    await conn.set_type_codec(
+            'json',
+            encoder=json.dumps,
+            decoder=json.loads,
+            schema='pg_catalog'
+        )
+    # try:
+    #     await conn.execute('''
+    #         DROP TABLE properties
+    #     ''')
+    #     await conn.execute('''
+    #         CREATE TABLE properties(
+    #                 cid serial,
+    #                 data json
+    #         )
+    #     ''')
+    # except:
+    #     pass
+    
+    data = await conn.fetchval('SELECT data FROM properties WHERE cid=$1', cid)
+    if data:
+        return data
 
     async with aiohttp.ClientSession() as session:
         async with session.get(
             f"https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/{cid}/JSON/",
         ) as response:
-            data = await response.json(encoding="Windows-1252")
+            data = await response.json(encoding="CP866")
     output_data = {}
     output_data["CID"] = cid
     output_data["Name"] = data["Record"]["RecordTitle"]
@@ -37,10 +62,18 @@ async def get_properties_from_cid(cid: int) -> dict:
                                     output_data["Properties"][subsubsection["TOCHeading"]].append(information["Value"]["Number"][0])
                             except:
                                 pass
+
+    await conn.execute('''
+        INSERT INTO properties(cid, data) VALUES($1, $2::json)
+    ''', cid, output_data)
     return output_data
 
 @app.get("/properties/{nametype}/{name}")
 async def get_properties(nametype: Literal["name", "smiles", "cid"], name: str):
+    
+    if nametype == "cid":
+        return await get_properties_from_cid(int(name))
+
     async with aiohttp.ClientSession() as session:
         async with session.get(f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/{nametype}/{name}/cids/json") as response:
             data = await response.json(encoding="Windows-1252")
