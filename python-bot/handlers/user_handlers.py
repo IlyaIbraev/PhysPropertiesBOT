6ce4356666a413ff89129e759db943f16828dd11
Ilya Ibraev <shilya91@gmail.com>
2023-12-21 03:16:49 +0300

    adding comments, removing unused things, improving stability

--- python-bot/handlers/user_handlers.py
+++ python-bot/handlers/user_handlers.py
@@ -11,87 +11,78 @@ from filters.registration_filter import RegistrationForm
 
 router = Router()
 
-
 # /start
 @router.message(CommandStart())
 async def process_start_command(message: Message, state: FSMContext):
-    await message.answer(text=str(await state.get_state()))
-    await state.set_state(RegistrationForm.registration_completed)
     await state.update_data(nametype="name")
+    await state.set_state(RegistrationForm.registration_completed)
     await message.answer(text=LEXICON_RU['/start'])
-    await message.answer(text=str(await state.get_data()))
-    await message.answer(text=str(await state.get_state()))
 
 # registration check
 @router.message(~StateFilter(RegistrationForm.registration_completed))
 async def process_nonregister(message: Message, state: FSMContext):
-    await message.answer("reg check")
-    await message.answer(text=str(await state.get_data()))
+    await message.answer(text=LEXICON_RU['unregistered'])
 
 # /help
 @router.message(Command(commands='help'))
 async def process_help_command(message: Message, state: FSMContext):
     await message.answer(text=LEXICON_RU['/help'])
-    await message.answer(text=str(await state.get_data()))
 
 # /name
 @router.message(Command(commands="name"))
 async def process_name_command(message: Message, state: FSMContext):
     await state.update_data(nametype="name")
     await message.answer(text=LEXICON_RU["/name"])
-    await message.answer(text=str(await state.get_data()))
 
 # /smiles
 @router.message(Command(commands="smiles"))
 async def process_name_command(message: Message, state: FSMContext):
     await state.update_data(nametype="smiles")
     await message.answer(text=LEXICON_RU["/smiles"])
-    await message.answer(text=str(await state.get_data()))
 
-# Название
+# Отработчик названий
 @router.message(F.text[0] != "/")
 async def process_get_properties(message: Message, state: FSMContext):
+    # Заявленное название вещества
     name = message.text
+    # Получение информации том, какой nametype выбран у пользователя
     user_data = await state.get_data()
     nametype = user_data["nametype"]
+    # Получение информации по веществу и статусу ответа
     response_data, status = get_properties(nametype, name)
-
-    # properties = response_data["Properties"]
-    molecule_name = response_data["Name"]
-    molecule_image = response_data["Image"]
-
-    molecule_keyboard = create_molecule_keyboard(response_data=response_data)
-
-    print(status, type(status))
+    
+    # Обработка статуса
     match status:
         case 200:
+            molecule_name = response_data["Name"]
+            molecule_image = response_data["Image"]
+            molecule_keyboard = create_molecule_keyboard(response_data=response_data)
             await message.answer(
-                text="{}\n{}".format(molecule_name, molecule_image),
+                text=LEXICON_RU["finder_200"].format(molecule_name, molecule_image),
                 reply_markup=molecule_keyboard
                 )
         case 400:
-            await message.answer("error")
+            await message.answer(text=LEXICON_RU["finder_400"])
         case 404:
-            await message.answer("not found")
-            
+            await message.answer(text=LEXICON_RU["finder_404"])
+
+# callback r"select:+"
 @router.callback_query(F.data.startswith("select:"))
 async def process_select_callback(callback: CallbackQuery, state: FSMContext, bot: Bot):
-    
-    response_data, status = get_properties("cid", callback.data.split(":")[1])
-
+    # Получение информации по веществу по CID
+    response_data, _ = get_properties("cid", callback.data.split(":")[1])
+    # Полученеи свойств вещества
     property = response_data["Properties"][callback.data.split(":")[2]]
-
+    # Формирование ответа
     answer = [callback.data.split(":")[2]]
-
     for elem in property:
         answer.append(elem)
-
     answer = "\n   ".join(map(str, answer))
         
-
-    # await callback.answer(text=str(answer), show_alert=True)
+    # Попытка отправить ответ на запрос алертом
     try:
         await callback.answer(answer, show_alert=True)
+    # Иначе ответ отправляется сообщением.
     except:
-        await callback.answer("Response too long to show as alert")
+        await callback.answer(text=LEXICON_RU["answer_too_long"])
         await bot.send_message(chat_id=callback.from_user.id, text=answer)
